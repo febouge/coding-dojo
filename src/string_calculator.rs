fe40c51d45b5316a2c69a17098a6097dd2f91933
Ferran <febouge@protonmail.ch>
2017-09-13 21:59:28 +0200

    Forth step: some refactor

--- src/string_calculator.rs
+++ src/string_calculator.rs
@@ -3,15 +3,15 @@ struct StringCalculator;
 
 impl StringCalculator {
     pub fn new() -> StringCalculator {
-        StringCalculator{}
+        StringCalculator {}
     }
 
     pub fn add(&self, input: &str) -> u32 {
-        if input == "" {
+        if self.is_empty_string(input) {
             return 0;
         }
         let result: u32;
-        if input.len() == 1 {
+        if self.is_just_one_number(input) {
             result = input.parse().unwrap();
         } else {
             let numbers: Vec<&str> = input.split(",").collect();
@@ -22,8 +22,19 @@ impl StringCalculator {
     }
 
     fn sum_string_numbers(&self, string_numbers: &Vec<&str>) -> u32 {
-        let parsed_number: Vec<u32> = string_numbers.into_iter().map(|string_number| string_number.parse().unwrap()).collect();
+        let parsed_number: Vec<u32> = string_numbers
+            .into_iter()
+            .map(|string_number| string_number.parse().unwrap())
+            .collect();
         parsed_number.into_iter().fold(0, |sum, x| sum + x)
+    }
+
+    fn is_empty_string(&self, input: &str) -> bool {
+        input == ""
+    }
+
+    fn is_just_one_number(&self, input: &str) -> bool {
+        input.len() == 1
     }
 }
 
@@ -34,7 +45,7 @@ mod tests {
     fn get_string_calculator() -> StringCalculator {
         StringCalculator::new()
     }
-    
+
     #[test]
     fn empty_string_should_return_zero() {
         assert_eq!(0, get_string_calculator().add(""));
@@ -44,15 +55,21 @@ mod tests {
     fn one_number_string_should_return_the_number() {
         assert_eq!(1, get_string_calculator().add("1"));
     }
-    
+
     #[test]
     fn two_numbers_comma_delimited_string_should_return_the_sum() {
         assert_eq!(3, get_string_calculator().add("1,2"));
     }
-    
+
     #[test]
     fn more_number_comma_delimited_string_should_return_the_sum() {
         assert_eq!(6, get_string_calculator().add("1,2,3"));
         assert_eq!(10, get_string_calculator().add("1,2,3,4"));
+    }
+
+    #[test]
+    fn numbers_comma_or_newline_delimited_string_should_return_the_sum() {
+        assert_eq!(6, get_string_calculator().add("1\n2,3"));
+        assert_eq!(10, get_string_calculator().add("1,2\n3\n4"));
     }
 }
